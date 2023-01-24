aca5f82ab08b855910b19fd4df9206db98013cff
Joe <joe@complex64.com>
2023-01-24 19:16:51 +0100

    init version 0.0.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 // Package version records versioning information about this module.
 package version
 
-var Version string
+var Version = "0.0.0"
 
 // String formats the semantic version string for this module.
 func String() string {
